44f9cfc01a0511ad0dedddd922903df6a01c51da
Johan Smith Agudelo Rodriguez <audoban@openmailbox.org>
2016-12-25 15:37:12 -0500

    fake custom target, and qmllint
    It is a small trap for that qtcreator show qml|js files, and we can use qmllint before the each build, and avoid some syntax errors.

---
+++ FakeTarget.cmake
@@ -0,0 +1,34 @@
+execute_process(COMMAND find plasmoid containment -name "*.qml" -o -name "*.js"
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+    OUTPUT_VARIABLE QML_SRCS_STRING)
+
+string(REPLACE "\n" ";" QML_SRCS ${QML_SRCS_STRING})
+
+# fake target for QtCreator project
+add_custom_target(fake-target
+    SOURCES ${QML_SRCS}
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR})
+
+# qmllint: qml static syntax checker
+if(${CMAKE_BUILD_TYPE} MATCHES "Debug")
+    find_program(QMLLINT qmllint)
+
+    if(EXISTS "${QMLLINT}")
+        message("-- Found qmllint: ${QMLLINT}")
+        add_custom_command(TARGET candil PRE_BUILD
+            COMMAND ${QMLLINT} ${QML_SRCS}
+            WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+            COMMENT "Running qmllint")
+    else()
+        message("-- qmllint: QML Syntax verifier not found")
+    endif()
+
+    message("-- Enabling QML debugging and profiling")
+    add_definitions(-DQT_QML_DEBUG)
+    add_definitions(-DQT_FATAL_WARNINGS)
+
+elseif(${CMAKE_BUILD_TYPE} MATCHES "Release")
+    message("-- Disabling debug info")
+    add_definitions(-DQT_NO_DEBUG)
+
+endif()
