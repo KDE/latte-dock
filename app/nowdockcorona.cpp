364c19a20516b6d5768f7def74fcb432b09f0a53
Michail Vourlakos <mvourlakos@gmail.com>
2016-12-29 00:33:43 +0200

    fix #8, block dockview creation for systray

--- app/nowdockcorona.cpp
+++ app/nowdockcorona.cpp
@@ -159,11 +159,11 @@ void NowDockCorona::addDock(Plasma::Containment *containment)
 
     // the system tray is a containment that behaves as an applet
     // so a dockview shouldnt be created for it
-    //KPluginMetaData metadata = containment->pluginMetaData();
+    KPluginMetaData metadata = containment->pluginMetaData();
 
-    //if (metadata.pluginId() == "org.kde.plasma.systemtray") {
-    //   return;
-    //}
+    if (metadata.pluginId() == "org.kde.plasma.systemtray") {
+        return;
+    }
 
     foreach (NowDockView *dock, m_containments) {
         if (dock->containment() == containment) {
