433f6a635248727f5eaadf012173db75b712e386
audoban <audoban@openmailbox.org>
2016-12-30 02:13:33 -0500

    static method for get a instance of abstractwindowinterface
    NOTE: WaylandWindowInterface has not been implemented

--- app/abstractwindowinterface.cpp
+++ app/abstractwindowinterface.cpp
@@ -1,7 +1,10 @@
 #include "abstractwindowinterface.h"
+#include "xwindowinterface.h"
 
 #include <QObject>
 #include <QQuickWindow>
+
+#include <KWindowSystem>
 
 namespace Latte {
 
@@ -13,9 +16,18 @@ AbstractWindowInterface::AbstractWindowInterface(QQuickWindow *const view, QObje
 
 AbstractWindowInterface::~AbstractWindowInterface()
 {
-
+    
 }
 
-
+AbstractWindowInterface *AbstractWindowInterface::getInstance(QQuickWindow * const view, QObject *parent)
+{
+    if (KWindowSystem::isPlatformWayland()) {
+        //! TODO: WaylandWindowInterface
+        return nullptr;
+    }
+    
+    /* if(KWindowSystem::isPlatformX11) */
+    return new XWindowInterface(view, parent);
+}
 
 }
