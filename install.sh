dbc2905077e73566157d32b6a62dc878abb017b7
Johan Smith Agudelo Rodriguez <audoban@openmailbox.org>
2017-01-03 18:32:32 -0500

    we can pass a build type to install.sh

--- install.sh
+++ install.sh
@@ -4,11 +4,14 @@
 #This script was written and tested on openSuSe Leap 42.1
 set -e
 
+build_type=$1
+build_type=${build_type:="Release"}
+
 if ! [ -a build ] ; then
     mkdir build
 fi
 
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=RELEASE ..
+cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=$build_type ..
 make
 sudo make install
