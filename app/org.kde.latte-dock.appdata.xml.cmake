cb5e7a830785e2a98e805cd1907860de5084e09d
Michail Vourlakos <mvourlakos@gmail.com>
2017-05-13 00:10:47 +0300

    fix #447,add an appstream file for Latte

---
+++ app/org.kde.latte-dock.appdata.xml.cmake
@@ -0,0 +1,46 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<component type="desktop">
+    <id>org.kde.latte-dock.desktop</id>
+    <name>Latte</name>
+    <summary>Dock for the masses</summary>
+    <description>
+        <p>Latte is a dock based on plasma frameworks that provides an elegant and intuitive experience for your tasks and plasmoids. It animates its contents by using parabolic zoom effect and trys to be there only when it is needed.</p>
+        <p> "Art In Coffee" </p>
+    </description>
+    <categories>
+        <category>Utility</category>
+        <category>KDE</category>
+    </categories>
+    <url type="homepage">@WEBSITE@</url>
+    <url type="bugtracker">@BUG_ADDRESS@</url>
+    <url type="faq">@FAQS@</url>    
+    <developer_name>@AUTHOR@</developer_name>
+    <icon type="stock">latte-dock</icon>
+    <project_license>GPL v2+</project_license>
+    <metadata_license>CC0-1.0</metadata_license>
+    <screenshots>
+        <screenshot type="default">
+            <image type="source">http://i.imgur.com/LLoXdgK.png</image>
+        </screenshot>
+        <screenshot>
+            <image type="source">https://2.bp.blogspot.com/-_gEFl6vIQt8/WNf3LaHcpvI/AAAAAAAAASk/CRZCF1vD2k0MQCesQk8CheBm5QrFRqYdQCLcB/s640/blur2.png</image>
+        </screenshot>         
+        <screenshot>
+            <image type="source">http://i.imgur.com/CHCZ51A.png</image>
+        </screenshot>
+        <screenshot>
+            <image type="source">https://3.bp.blogspot.com/-4u-K09Tu8xI/WNf3LifOFxI/AAAAAAAAASo/AKh2HcenWpovNoJM8Yy65_jZUoXfhEi_QCLcB/s400/configwin.png</image>
+        </screenshot>
+  </screenshots>
+  <project_group>KDE</project_group>
+  <provides>
+    <binary>latte-dock</binary>
+    <library>liblattedockplugin.so</library>
+  </provides>
+  <releases>
+​    <release version="0.6.0" date="2017-04-02" />
+​    <release version="0.6.1" date="2017-05-05" />
+​    <release version="0.6.2" date="2017-05-09" />
+​  </releases>  
+</component>
+ 
