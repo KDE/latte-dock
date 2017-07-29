6893ef928784b5b72ab0214a31f4e1521c752b9f
Michail Vourlakos <mvourlakos@gmail.com>
2017-07-29 10:38:05 +0300

    fix #627,remove empty values from desktop file
    --some empty values in strings was breaking the translation
    chain through the update-metadata script

--- app/org.kde.latte-dock.desktop.cmake
+++ app/org.kde.latte-dock.desktop.cmake
@@ -1,6 +1,13 @@
 [Desktop Entry]
-Categories=Utility;X-SuSE-DesktopUtility;
-Comment[el_GR]=Dock for the masses
+Name[de]=Latte
+Name[el]=Latte
+Name[es]=Latte
+Name[it]=Latte
+Name[pl]=Latte
+Name[pt]=Latte
+Name[ru]=Латте
+Name[zh_TW]=Latte
+Name=Latte
 Comment[de]=Dock für die Massen
 Comment[el]=Πίνακας για όλον τον κόσμο
 Comment[es]=Dock para las masas
@@ -10,8 +17,6 @@ Comment[pt]=Dock para as massas
 Comment[ru]=Док в массы
 Comment[zh_TW]=給大眾使用的 dock
 Comment=Dock for the masses
-Exec=latte-dock %u
-GenericName[el_GR]=Dock
 GenericName[de]=Dock
 GenericName[el]=Πίνακας
 GenericName[es]=Dock
@@ -22,20 +27,12 @@ GenericName[ru]=Док
 GenericName[zh_TW]=Dock
 GenericName=Dock
 Icon=latte-dock
+Categories=Utility;X-SuSE-DesktopUtility;
+Exec=latte-dock %u
 InitialPreference=1
-MimeType=Name[el_GR]=Latte
-Name[de]=Latte
-Name[el]=Latte
-Name[es]=Latte
-Name[it]=Latte
-Name[pl]=Latte
-Name[pt]=Latte
-Name[ru]=Латте
-Name[zh_TW]=Latte
-Name=Latte
-Path=StartupNotify=false
+StartupNotify=false
 Terminal=false
-TerminalOptions=Type=Application
+Type=Application
 Version=@VERSION@
 X-DBUS-ServiceName=org.kde.lattedock
 X-DBUS-StartupType=unique
@@ -44,4 +41,4 @@ X-KDE-PluginInfo-Email=@EMAIL@
 X-KDE-PluginInfo-Website=@WEBSITE@
 X-KDE-StartupNotify=false
 X-KDE-SubstituteUID=false
-X-KDE-Username=
+
