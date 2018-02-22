c573f8f0c189d4274ab7b17bb9f339b4d816d114
l10n daemon script <scripty@kde.org>
2018-02-22 05:56:04 +0100

    GIT_SILENT made messages (after extraction)

--- app/org.kde.latte-dock.appdata.xml.cmake
+++ app/org.kde.latte-dock.appdata.xml.cmake
@@ -14,6 +14,7 @@
   <name xml:lang="nl">Latte</name>
   <name xml:lang="pl">Latte</name>
   <name xml:lang="pt">Latte</name>
+  <name xml:lang="pt-BR">Latte</name>
   <name xml:lang="ru">Latte</name>
   <name xml:lang="sk">Latte</name>
   <name xml:lang="sv">Latte</name>
@@ -72,6 +73,7 @@
     <p xml:lang="nl">"Kunst in koffie"</p>
     <p xml:lang="pl">"Sztuka w Kawie"</p>
     <p xml:lang="pt">"Art In Coffee"</p>
+    <p xml:lang="pt-BR">"Arte no café"</p>
     <p xml:lang="ru">«Искусство в кофе»</p>
     <p xml:lang="sk">"Umenie v káve"</p>
     <p xml:lang="sv">"Konst i kaffe"</p>
