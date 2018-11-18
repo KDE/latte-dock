6e2c645008cc812802f9633a5a0ef1c316cb93c8
Michail Vourlakos <mvourlakos@gmail.com>
2018-11-18 20:57:29 +0200

    Revert "Revert "No need to search i18n() in the same folder twice""
    This reverts commit 1888fb7c8099461e6311b66508018cb963e1c264.
    
    the commiter was right this isnt needed, find returns the
    in question files in app/dock folder

--- app/Messages.sh
+++ app/Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 $EXTRACTRC `find . -name \*.rc -o -name \*.ui` >> rc.cpp
 
-$XGETTEXT `find . ../shell -name \*.js -o -name \*.qml -o -name \*.cpp -o -name ./dock\/\*.cpp` -o $podir/latte-dock.pot
+$XGETTEXT `find . ../shell -name \*.js -o -name \*.qml -o -name \*.cpp` -o $podir/latte-dock.pot
