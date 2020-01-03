d2170f13c04e13e392ae780ee28b60ddebb20c9a
Johnny Jazeix <jazeix@gmail.com>
2020-01-03 19:41:26 +0100

    revert previous commit, add KDECompilerSettings back

--- app/FakeTarget.cmake
+++ app/FakeTarget.cmake
@@ -10,21 +10,19 @@ add_custom_target(fake-target
     SOURCES ${QML_SRCS})
 
 # qmllint: qml static syntax checker
-if(${CMAKE_BUILD_TYPE} MATCHES "Debug")
-    if(NOT DEFINED ECM_ENABLE_SANITIZERS)
-        find_program(QMLLINT qmllint)
-        if(EXISTS "${QMLLINT}")
-            message("-- Found qmllint: ${QMLLINT}")
-            add_custom_command(TARGET latte-dock PRE_BUILD
-                COMMAND ${QMLLINT} ${QML_SRCS}
-                WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-                COMMENT "Running qmllint")
-        else()
-            message("-- qmllint: QML Syntax verifier not found")
-        endif()
+if(${CMAKE_BUILD_TYPE} MATCHES "Debug" AND NOT DEFINED ECM_ENABLE_SANITIZERS)
+    find_program(QMLLINT qmllint)
+
+    if(EXISTS "${QMLLINT}")
+        message("-- Found qmllint: ${QMLLINT}")
+        add_custom_command(TARGET latte-dock PRE_BUILD
+            COMMAND ${QMLLINT} ${QML_SRCS}
+            WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+            COMMENT "Running qmllint")
     else()
-        message("-- Disable qmllint because ECM_ENABLE_SANITIZERS is defined")
+        message("-- qmllint: QML Syntax verifier not found")
     endif()
+
     message("-- Enabling QML debugging and profiling")
     add_definitions(-DQT_QML_DEBUG)
     add_definitions(-DQT_FATAL_WARNINGS)
