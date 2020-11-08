54a14bce0f5db846b183d1653f0f71a9369fdf5c
Vlad Zahorodnii <vlad.zahorodnii@kde.org>
2020-11-08 16:48:00 +0200

    wayland: Add the screencast protocol to X-KDE-Wayland-Interfaces
    Recently support for window thumbnails on Wayland was added in
    latte-dock, but it shouldn't work as is because the screencast protocol
    is not listed in X-KDE-Wayland-Interfaces. Only "trusted" applications
    can bind the screencast global.

--- app/org.kde.latte-dock.desktop.cmake
+++ app/org.kde.latte-dock.desktop.cmake
@@ -110,5 +110,5 @@ X-KDE-PluginInfo-Website=@WEBSITE@
 X-KDE-StartupNotify=false
 X-KDE-SubstituteUID=false
 X-KDE-autostart-phase=1
-X-KDE-Wayland-Interfaces=org_kde_plasma_window_management,org_kde_kwin_keystate
+X-KDE-Wayland-Interfaces=org_kde_plasma_window_management,org_kde_kwin_keystate,zkde_screencast_unstable_v1
 
