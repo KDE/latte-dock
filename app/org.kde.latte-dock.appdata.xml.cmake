035d12745398cf21540b1a8bf80331601f49df49
l10n daemon script <scripty@kde.org>
2021-03-31 01:42:35 +0000

    GIT_SILENT made messages (after extraction)

--- app/org.kde.latte-dock.appdata.xml.cmake
+++ app/org.kde.latte-dock.appdata.xml.cmake
@@ -27,6 +27,7 @@
   <name xml:lang="pt-BR">Latte</name>
   <name xml:lang="ru">Latte</name>
   <name xml:lang="sk">Latte</name>
+  <name xml:lang="sl">Latte</name>
   <name xml:lang="sv">Latte</name>
   <name xml:lang="uk">Латте</name>
   <name xml:lang="x-test">xxLattexx</name>
@@ -57,6 +58,7 @@
   <summary xml:lang="pt-BR">Área acoplável para todos</summary>
   <summary xml:lang="ru">Док в массы</summary>
   <summary xml:lang="sk">Dok pre masy</summary>
+  <summary xml:lang="sl">Dok za množice</summary>
   <summary xml:lang="sv">Dockningsfönster för alla</summary>
   <summary xml:lang="uk">Панель для усіх</summary>
   <summary xml:lang="x-test">xxDock for the massesxx</summary>
@@ -87,6 +89,7 @@
     <p xml:lang="pt-BR">O Latte é uma área acoplável baseada nos frameworks do Plasma que oferece uma experiência elegante e intuitiva para as suas tarefas e plasmoides. Ele anima o seu conteúdo, usando um efeito de zoom parabólico e tenta estar apenas onde for necessário.</p>
     <p xml:lang="ru">Latte — это док-панель, основанная на KDE Frameworks, которая обеспечивает элегантный и интуитивно понятный интерфейс для задач и виджетов Plasma. Для анимация содержимого используется эффект параболического увеличения. Панель пытается быть видима только тогда, когда это необходимо.</p>
     <p xml:lang="sk">Latte je dok založený na frameworkoch plasma, ktorý poskytuje elegantné a intuitívne rozhranie pre vaše úlohy a plazmoidy. Animuje svoj obsah použitím parabolického efektu priblíženia a snaží sa by tam len vtedy, keď je to treba.</p>
+    <p xml:lang="sl">Latte je dok, ki temelji v okolju Plasma Frameworka, ki zagotavlja elegantno in intuitivno izkušnjo za vaše naloge in plazmoide. Svojo vsebino animiraz uporabo učinka paraboličnega povečevanja in poskuša biti tam le, kadar je to potrebno.</p>
     <p xml:lang="sv">Latte är ett dockningsfönster baserat på Plasma ramverk som erbjuder en elegant och intuitiv upplevelse för aktiviteter och Plasmoider. Det animerar innehållet genom att använda en paraboleffekt för zoomning, och försöker bara vara där när det behövs.</p>
     <p xml:lang="uk">Латте — панель на основі бібліотек Плазми, яка забезпечує елегантний і інтуїтивно зрозумілий доступ до ваших завдань та плазмоїдів. Вміст панелі анімується за допомогою ефекту параболічного збільшення і показується лише тоді, коли це потрібно.</p>
     <p xml:lang="x-test">xxLatte is a dock based on plasma frameworks that provides an elegant and intuitive experience for your tasks and plasmoids. It animates its contents by using parabolic zoom effect and tries to be there only when it is needed.xx</p>
@@ -116,6 +119,7 @@
     <p xml:lang="pt-BR">"Arte no café"</p>
     <p xml:lang="ru">«Искусство в кофе»</p>
     <p xml:lang="sk">"Umenie v káve"</p>
+    <p xml:lang="sl">"Umetnost v kavi"</p>
     <p xml:lang="sv">"Konst i kaffe"</p>
     <p xml:lang="uk">«Мистецтво кави»</p>
     <p xml:lang="x-test">xx"Art In Coffee"xx</p>
@@ -153,6 +157,7 @@
   <developer_name xml:lang="pt-BR">Michail Vourlakos, Johan Smith Agudelo Rodriguez</developer_name>
   <developer_name xml:lang="ru">Michail Vourlakos, Johan Smith Agudelo Rodriguez</developer_name>
   <developer_name xml:lang="sk">Michail Vourlakos, Johan Smith Agudelo Rodriguez</developer_name>
+  <developer_name xml:lang="sl">Michail Vourlakos, Johan Smith Agudelo Rodriguez</developer_name>
   <developer_name xml:lang="sv">Michail Vourlakos, Johan Smith Agudelo Rodriguez</developer_name>
   <developer_name xml:lang="uk">Michail Vourlakos, Johan Smith Agudelo Rodriguez</developer_name>
   <developer_name xml:lang="x-test">xxMichail Vourlakos, Johan Smith Agudelo Rodriguezxx</developer_name>
