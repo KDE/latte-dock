42bd6b8f8a43e860ae9116087dea3438d1597760
Michail Vourlakos <mvourlakos@gmail.com>
2021-06-06 13:15:37 +0300

    data:introduce contextmenudata class
    --this definition is scheduled to be shared
    between the application and the context menu

---
+++ app/data/contextmenudata.h
@@ -0,0 +1,46 @@
+/*
+    SPDX-FileCopyrightText: 2021 Michail Vourlakos <mvourlakos@gmail.com>
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#ifndef CONTEXTMENUDATA_H
+#define CONTEXTMENUDATA_H
+
+// Qt
+#include <QStringList>
+
+namespace Latte {
+namespace Data {
+namespace ContextMenu {
+
+static const char *ADDVIEWACTION= "_add_view";
+static const char *LAYOUTSACTION[] = "_layouts";
+static const char *PREFERENCESACTION[] = "_preferences";
+static const char *QUITLATTEACTION[] = "_quit_latte";
+static const char *ADDWIDGETSACTION[] = "_add_latte_widgets";
+static const char *DUPLICATEVIEWACTION[] = "_duplicate_view";
+static const char *EDITVIEWACTION[] = "_edit_view";
+static const char *EXPORTVIEWTEMPLATEACTION[] = "_export_view";
+static const char *REMOVEVIEWACTION[] = "_remove_view";
+static const char *MOVEVIEWACTION[] = "_move_view";
+static const char *SEPARATOR1ACTION[] = "_separator1";
+
+static QStringList ACTIONSALWAYSVISIBLE = {LAYOUTSACTION,
+                                           PREFERENCESACTION,
+                                           QUITLATTEACTION,
+                                           SEPARATOR1ACTION,
+                                           ADDWIDGETSACTION,
+                                           ADDVIEWACTION,
+                                           MOVEVIEWACTION,
+                                           EXPORTVIEWTEMPLATEACTION,
+                                           REMOVEVIEWACTION}
+
+static QStringList ACTIONSVISIBLEONLYINEDIT = {MOVEVIEWACTION,
+                                               EXPORTVIEWTEMPLATEACTION,
+                                               REMOVEVIEWACTION};
+
+}
+}
+}
+
+#endif
