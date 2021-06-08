cbc0ba02e5bf4d4fbb9db58b93c279b84a4b897e
Michail Vourlakos <mvourlakos@gmail.com>
2021-06-08 21:57:40 +0300

    settings:provide actionlistwidgetitem

---
+++ app/settings/settingsdialog/actionlistwidgetitem.cpp
@@ -0,0 +1,30 @@
+/*
+    SPDX-FileCopyrightText: 2021 Michail Vourlakos <mvourlakos@gmail.com>
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "actionlistwidgetitem.h"
+
+
+namespace Latte {
+namespace Settings {
+namespace Preferences {
+
+ActionListWidgetItem::ActionListWidgetItem(const QIcon &icon, const QString &text, const int &order, const QString &id, QListWidget *parent, int type)
+    : QListWidgetItem(icon, text, parent, type)
+{
+    setData(IDROLE, id);
+    setData(ORDERROLE, order);
+}
+
+bool ActionListWidgetItem::operator<(const QListWidgetItem &other) const
+{
+    int curorder = data(ORDERROLE).toInt();
+    int othorder = other.data(ORDERROLE).toInt();
+
+    return (curorder < othorder);
+}
+
+}
+}
+}
---
+++ app/settings/settingsdialog/actionlistwidgetitem.h
@@ -0,0 +1,33 @@
+/*
+    SPDX-FileCopyrightText: 2021 Michail Vourlakos <mvourlakos@gmail.com>
+    SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#ifndef ACTIONLISTWIDGETITEM_H
+#define ACTIONLISTWIDGETITEM_H
+
+// Qt
+#include <QListWidgetItem>
+
+namespace Latte {
+namespace Settings {
+namespace Preferences {
+
+class ActionListWidgetItem : public QListWidgetItem
+{
+public:
+    enum ActionUserRoles
+    {
+        IDROLE = Qt::UserRole + 1,
+        ORDERROLE
+    };
+
+    ActionListWidgetItem(const QIcon &icon, const QString &text, const int &order, const QString &id, QListWidget *parent = nullptr, int type = QListWidgetItem::Type);
+
+    bool operator<(const QListWidgetItem &other) const;
+};
+
+}
+}
+}
+#endif
