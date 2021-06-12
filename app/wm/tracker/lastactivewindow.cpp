2aec29a85555edda7aa2e29a25798cf44344eccf
Michail Vourlakos <mvourlakos@gmail.com>
2021-06-12 17:35:16 +0300

    activate dragged window on start
    --this way the window is activated properly
    and its activation is not canceled from move
    action
    
    BUG:437679

--- app/wm/tracker/lastactivewindow.cpp
+++ app/wm/tracker/lastactivewindow.cpp
@@ -592,13 +592,13 @@ void LastActiveWindow::requestMove(Latte::View *fromView, int localX, int localY
         return;
     }
 
-    QPoint globalPoint{fromView->x() + localX, fromView->y() + localY};    
-    m_wm->requestMoveWindow(m_winId, globalPoint);
-
-    fromView->unblockMouse(localX, localY);
+    QPoint globalPoint{fromView->x() + localX, fromView->y() + localY};
 
     //! fixes bug #437679, Dragged windows do not become active and during drag they go behind active window
     m_wm->requestActivate(m_winId);
+
+    m_wm->requestMoveWindow(m_winId, globalPoint);
+    fromView->unblockMouse(localX, localY);
 }
 
 void LastActiveWindow::requestToggleIsOnAllDesktops()
