65a834a1aedf57d409f41b9e493d74bdbbda7dc6
l10n daemon script <scripty@kde.org>
2025-09-09 02:40:39 +0000

    GIT_SILENT made messages (after extraction)

--- app/org.kde.latte-dock.appdata.xml.cmake
+++ app/org.kde.latte-dock.appdata.xml.cmake
@@ -36,7 +36,6 @@
   <name xml:lang="sv">Latte</name>
   <name xml:lang="tr">Latte</name>
   <name xml:lang="uk">Латте</name>
-  <name xml:lang="x-test">xxLattexx</name>
   <name xml:lang="zh-CN">Latte</name>
   <name xml:lang="zh-TW">Latte</name>
   <summary>Dock for the masses</summary>
@@ -71,7 +70,6 @@
   <summary xml:lang="sv">Dockningsfönster för alla</summary>
   <summary xml:lang="tr">Kitleler için rıhtım</summary>
   <summary xml:lang="uk">Панель для усіх</summary>
-  <summary xml:lang="x-test">xxDock for the massesxx</summary>
   <summary xml:lang="zh-CN">符合大多数人使用习惯的停靠栏程序</summary>
   <summary xml:lang="zh-TW">給大眾使用的 Dock</summary>
   <description>
@@ -106,7 +104,6 @@
     <p xml:lang="sv">Latte är ett dockningsfönster baserat på Plasma ramverk som erbjuder en elegant och intuitiv upplevelse för aktiviteter och Plasmoider. Det animerar innehållet genom att använda en paraboleffekt för zoomning, och försöker bara vara där när det behövs.</p>
     <p xml:lang="tr">Latte, görevleriniz ve Plasmoid'leriniz için zarif ve sezgisel bir deneyim sağlayan Plasma kod kitaplıklarını kullanan bir rıhtımdır. İçeriğini parabolik yakınlaştırma efekti kullanarak canlandırır ve yalnızca gereksinim duyulduğunda orada olmaya çalışır.</p>
     <p xml:lang="uk">Латте — панель на основі бібліотек Плазми, яка забезпечує елегантний і інтуїтивно зрозумілий доступ до ваших завдань та плазмоїдів. Вміст панелі анімується за допомогою ефекту параболічного збільшення і показується лише тоді, коли це потрібно.</p>
-    <p xml:lang="x-test">xxLatte is a dock based on plasma frameworks that provides an elegant and intuitive experience for your tasks and plasmoids. It animates its contents by using parabolic zoom effect and tries to be there only when it is needed.xx</p>
     <p xml:lang="zh-CN">Latte 是一款基于 Plasma 框架的停靠栏程序，它为您的任务和 Plasma 小程序提供了简洁优雅、直观易懂的使用体验。它能够以抛物线缩放动效显示其内容，并尽可能在不需要的时候降低存在感，减少对用户的干扰。</p>
     <p xml:lang="zh-TW">Latte 是一個基於 Plasma 框架並提供您的工作與 plasmoid 優雅與直觀體驗的 dock。它使用拋物線縮放效果來讓您的內容動畫化，並試著只在您需要它時出現。</p>
     <p> "Art In Coffee" </p>
@@ -142,7 +139,6 @@
     <p xml:lang="sv">"Konst i kaffe"</p>
     <p xml:lang="tr">"Kahvedeki Sanat"</p>
     <p xml:lang="uk">«Мистецтво кави»</p>
-    <p xml:lang="x-test">xx"Art In Coffee"xx</p>
     <p xml:lang="zh-CN">“咖啡中的艺术”</p>
     <p xml:lang="zh-TW">「咖啡中的藝術」</p>
   </description>
@@ -186,7 +182,6 @@
   <developer_name xml:lang="sv">Michail Vourlakos, Johan Smith Agudelo Rodriguez</developer_name>
   <developer_name xml:lang="tr">Michail Vourlakos, Johan Smith Agudelo Rodriguez</developer_name>
   <developer_name xml:lang="uk">Michail Vourlakos, Johan Smith Agudelo Rodriguez</developer_name>
-  <developer_name xml:lang="x-test">xxMichail Vourlakos, Johan Smith Agudelo Rodriguezxx</developer_name>
   <developer_name xml:lang="zh-CN">Michail Vourlakos, Johan Smith Agudelo Rodriguez</developer_name>
   <developer_name xml:lang="zh-TW">Michail Vourlakos, Johan Smith Agudelo Rodriguez</developer_name>
   <icon type="stock">latte-dock</icon>
